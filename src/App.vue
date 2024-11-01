859ad78ba53aa152f2f82e5f587ac6acf7a80c38
nguyenkien <nguyenkienynk@gmail.com>
2024-11-01 11:18:13 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 <template>
-<!--  <RouterLink class="nav-link" :to="{ name: 'bai1-buoi4' }"></RouterLink>-->
-<!--  <RouterView />-->
+  <RouterLink class="nav-link" :to="{ name: 'bai1-buoi4' }"></RouterLink>
+  <RouterView />
 </template>
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import Bai1 from '@/views/buoi-4/bai-1.vue'
+import Bai1 from '@/views/buoi-4/bai-1.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/bai1-buoi4',
-    //   name: 'bai1-buoi4',
-    //   component: Bai1,
-    // }
+    {
+      path: '/bai1-buoi4',
+      name: 'bai1-buoi4',
+      component: Bai1,
+    }
   ]
 })
 
